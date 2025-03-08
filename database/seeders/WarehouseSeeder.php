e46f44ca3c932428e303af376dce16cf8a6ec19a
Ernesto Canquiz <caribestic123@gmail.com>
2025-03-08 13:35:20 -0400

    feat: Seeds with information from the articles API

--- database/seeders/WarehouseSeeder.php
+++ database/seeders/WarehouseSeeder.php
@@ -18,9 +18,9 @@ class WarehouseSeeder extends Seeder
         $uuid2=(string)Str::uuid();
         $uuid3=(string)Str::uuid();
         
-        DB::statement("INSERT INTO public.warehouses(uuid, name, description, categories, created_at, updated_at) VALUES ('$uuid1','Almacén Uno', 'Almacén Uno, Almacén Uno, Almacén Uno', '[\"PINTURAS / INTERIORES\", \"PINTURAS / EXTERIORES\", \"PINTURAS / OTROS\"]', now(), now())");
-        DB::statement("INSERT INTO public.warehouses(uuid, name, description, categories, created_at, updated_at) VALUES ('$uuid2','Almacén Dos', 'Almacén Dos, Almacén Dos, Almacén Dos', '[\"PRODUCTOS DE LIMPIEZA / ASEO PERSONAL\", \"PRODUCTOS DE LIMPIEZA / HIGIENE DEL HOGAR\"]', now(), now())");
-        DB::statement("INSERT INTO public.warehouses(uuid, name, description, categories, created_at, updated_at) VALUES ('$uuid3','Almacén Tres', 'Almacén Tres, Almacén Tres,Almacén Tres', '[\"HERRAMIENTAS\"]', now(), now())");
+        DB::statement("INSERT INTO public.warehouses(uuid, name, description, categories, created_at, updated_at) VALUES ('$uuid1','Principal', 'Almacén Principal o Genetal', '[\"HERRAMIENTAS\", \"PINTURAS / EXTERIORES\", \"PINTURAS / INTERIORES\", \"PINTURAS / OTROS\", \"PRODUCTOS DE LIMPIEZA / ASEO PERSONAL\", \"PRODUCTOS DE LIMPIEZA / HIGIENE DEL HOGAR\"]', now(), now())");
+        DB::statement("INSERT INTO public.warehouses(uuid, name, description, categories, created_at, updated_at) VALUES ('$uuid2','Productos de Limpieza', 'Almacén de Productos de Limpieza', '[\"PRODUCTOS DE LIMPIEZA / ASEO PERSONAL\", \"PRODUCTOS DE LIMPIEZA / HIGIENE DEL HOGAR\"]', now(), now())");
+        DB::statement("INSERT INTO public.warehouses(uuid, name, description, categories, created_at, updated_at) VALUES ('$uuid3','Pinturas y Herramientas', 'Almacén de Pinturas y Herramientas', '[\"HERRAMIENTAS\", \"PINTURAS / EXTERIORES\", \"PINTURAS / INTERIORES\", \"PINTURAS / OTROS\"]', now(), now())");
 
     }
 }
